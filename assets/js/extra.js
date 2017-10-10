2ea0e186859bf17d3bada56a81c515b3a6d8558e
EnMediokerLyktstolpe <30979495+EnMediokerLyktstolpe@user.noreply.github.com>
2017-10-10 19:04:29 +0200

    new product page

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -87,19 +87,3 @@ $('.tile')
 //--------------------------------------------//
 //--------------ADAMS Modal   ----------------//
 //--------------------------------------------//
-
-
-// Get the modal
-var modal1 = document.getElementById('modal1');
-// Get the button that opens the modal
-var btn1 = document.getElementById("openModalBtn1");
-// Get the <span> element that closes the modal
-var div1 = document.getElementById('closeModalBtn1');
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-div1.onclick = function() {
-    modal1.style.display = "none";
-}
